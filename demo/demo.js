87028dff9a5e205fcf9e06c8d9b9a4f3b66f1ce1
Sam-Mauris Yong <mauris@hotmail.sg>
2012-06-04 14:03:27 +0800

    demonstration implementation
    tap demo implementation

---
+++ demo/demo.js
@@ -0,0 +1,28 @@
+
+
+(function($){
+    
+    var TouchTapDemo = {
+        init: function(){
+            this.resetLabel();
+            this.setHandlers();
+        },
+        resetLabel: function(){
+            $(".example-unit").each(function(){
+                $(this).text($(this).attr('data-label'));
+            });
+        },
+        setTriggerLabel: function(e){
+            $(e).text($(e).attr('data-trigger'));
+        },
+        setHandlers: function(){
+            $("#exTap1").touchtap('tap', function(){
+                TouchTapDemo.setTriggerLabel(this);
+                window.setTimeout(function(){TouchTapDemo.resetLabel();}, 2000);
+            });
+        }
+    };
+    
+    TouchTapDemo.init();
+
+})(jQuery);
